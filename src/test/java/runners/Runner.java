ee87818793723cd933ed5b7535d21a231f06864f
abayramusa <techproed@techproeds-iMac.local>
2020-05-12 19:47:21 -0500

    commiting

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         plugin = "html:target/cucumber",
         features ="src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@wip2",
+        tags = "@wip1",
         dryRun = false
 )
 public class Runner {
--- src/test/resources/features/fluent wait.feature
+++
@@ -1,6 +0,0 @@
-@wip2
-Feature: fluent wait demo
-  Scenario: fluent wait scenario
-    Given user is on the demo page
-    And user clicks on the Start button
-    And user waits fluently until the page loads and verify the text message
